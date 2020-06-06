cb0c6d160e06a8d075e4803b3308969151ebf98c
nobuyuki.muranaka <nobumuranaka9365@gmail.com>
2020-06-06 08:05:41 +0000

    tasklist

--- database/migrations/2020_06_05_225754_add_status_to_tasks_table.php
+++ database/migrations/2020_06_05_225754_add_status_to_tasks_table.php
@@ -13,7 +13,7 @@ class AddStatusToTasksTable extends Migration
      */
     public function up()
     {
-        Schema::table('status', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $table) {
             $table->string('status', 10);
  
         });
@@ -26,7 +26,7 @@ class AddStatusToTasksTable extends Migration
      */
     public function down()
     {
-        Schema::table('status', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $table) {
             $table->dropColumn('status');
         });
     }
